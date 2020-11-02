990ba3b9921ac4813fd0fa5d96f050a8a3572810
Josh Taylor <joshuataylorx@gmail.com>
2020-11-02 13:16:47 +0800

    bump dbt

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,18 +22,3 @@ then
  sed -i "s/_password_/${DBT_PASSWORD}/g" ./profiles.yml
 fi
 $1
-# DBT_LOG_FILE=${DBT_LOG_FILE:="dbt_console_output.txt"}
-# DBT_LOG_PATH="${INPUT_DBT_PROJECT_FOLDER}/${DBT_LOG_FILE}"
-# echo "DBT_LOG_PATH=${DBT_LOG_PATH}" >> $GITHUB_ENV
-# echo "saving console output in \"${DBT_LOG_PATH}\""
-# $1 2>&1 | tee "${DBT_LOG_FILE}"
-# if [ $? -eq 0 ]
-#   then
-#     echo "DBT_RUN_STATE=passed" >> $GITHUB_ENV
-#     echo "::set-output name=result::passed"
-#     echo "DBT run OK" >> "${DBT_LOG_FILE}"
-#   else
-#     echo "DBT_RUN_STATE=failed" >> $GITHUB_ENV
-#     echo "::set-output name=result::failed"
-#     echo "DBT run failed" >> "${DBT_LOG_FILE}"
-# fi
